ddccf95e578d8475ea3d5c4ce2206baa69a0ba23
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-10-13 03:01:15 +0300

    Finished CLI & I/O

--- lab-1/src/calc.c
+++ lab-1/src/calc.c
@@ -12,49 +12,52 @@ int main(int argc, char *argv[])
     char *input_directory = NULL; 
     char *output_directory = NULL;
     DIR *folder;
-    for (int i = 0; i<argc;  i++)
+    //Loop to parse input direcotry commands
+    for (int i = 1; i<argc;  i++) //since arg[0] is always the program name, I'm starting the loop at 1
     {
         if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--dir") == 0) //since c does not treat strings like java, I couldn't do if (argv[i]. == "-d") or argv[i].equalsIgnoreCase("-d"))...
         {
             if (i + 1 < argc) //cheking for the DIR in [-d DIR]
             {
-                input_directory = argv[i+1]; //Storing the DIR in input_directory so that I can quickly reference it later
-                folder = opedir(input_directory); ////Logic for reading entire directory found here https://c-for-dummies.com/blog/?p=3246
-                struct dirent *entry; //dirent usage found here https://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html
-                while(entry = readdir(folder))
+                input_directory = argv[i+1];
+                folder = opendir(input_directory);
+                if (!folder)
                 {
-                    size_t length = strlen(entry->d_name);
-                    if(length >= 4 && strcmp(entry->d_name + length - 4, ".txt")) == 0)
-                    {
-                    //processing here
-                    //
-                    //
-                    //
-                    }
-            } else {} (strcmp) {
-                if (folder == NULL)
-                {
-                    puts("Unable to read the directory"); //Chose to use puts instead of printf as I've never used it and I don't need any text formatting. Plus puts adds a newline char at the end. 
-                    return 1; //Returning 1 to terminate program due to error
+                    pfrintf(stderr, "Error opening specified folder");
+                    return 1;
                 }
-
-                {
-                    perror("Unable to open the file");
-                    return 1; //Returning 1 to terminate program due to an error
-                }
-                printf ("You need to specify a directory after -d or --dir");
-            }
+                closedir(folder);
+                i++; //skipping a step 
+            } else {
+                pfrintf(stderr, "You need to specify a diretory after -d or --dir.\n");
+                return 1;
+            }    
         }
-    }  
+    //--------------------------------------------------------------------------//
     if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output-dir") == 0)
     {
         if (i + 1 < argc)
         {
             output_directory = argv[i+1];
+            folder = opendir(output_directory);
+            if (!folder)    
+            {
+                if (mkdir (output_directory, 0777) != 0)
+                {
+                    perror("Error creating the directory\n");
+                    return 1;
+                } else {
+                    printf("Successfully created directory: %s\n", output_directory);
+                }
+            } else {
+                closedir(folder);//directory already exists
+            }
         }
+       
     }
+}
+
     
-    folder = opendir(output_directory);
     
     if (output_directory == NULL) //Will create the default folder in the current working directory  
     {
@@ -62,6 +65,11 @@ int main(int argc, char *argv[])
         char studentid[20];
         printf("Enter your student ID: ");
         scanf("%19s", studentid);
+        if (input_directory == NULL)
+        {
+            fprintf(stderr, "Error: No input directory provided (use -d or --dir).\n");
+            return 1;
+        }
         char *last_slash = strrchr(input_directory, '/'); //finiding where the last slash is becasue input base follows that
         char input_base[256];
         if (last_slash)
@@ -75,22 +83,5 @@ int main(int argc, char *argv[])
         snprintf(new_dir, sizeof(new_dir), "%s_%s_%s", input_base, username, studentid); //using snprintf instead to prevent buffer overflow
         mkdir(new_dir, 0777); //mkdir logic found here https://www.delftstack.com/howto/c/mkdir-in-c/
         //Everyone has read and write permissions
-        if (mkdir(new_dir, 0777) != 0)
-        {
-            perror("Error creating output directory");
-            return 1;
-        }
-    } else {
-        folder = opendir(output_directory);
-        if (folder)
-        {
-            ///
-            ///
-            ///
-            closedir(folder);
-        } else {
-            mkdir (output_directory, 0777);
-        }
-    }
     return 0;  
 }
