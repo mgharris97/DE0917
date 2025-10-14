e476e2fea8f7b26dccd98af0adf52822b34ac876
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-10-14 23:15:06 +0300

    Finalized -o and -p logic

--- lab-1/src/calc.c
+++ lab-1/src/calc.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) 
 {
@@ -12,7 +14,7 @@ int main(int argc, char *argv[])
     char *input_directory = NULL; 
     char *output_directory = NULL;
     DIR *folder;
-    //Loop to parse input direcotry commands
+    //Loop to parse input and output  direcotry commands
     for (int i = 1; i<argc;  i++) //since arg[0] is always the program name, I'm starting the loop at 1
     {
         if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--dir") == 0) //since c does not treat strings like java, I couldn't do if (argv[i]. == "-d") or argv[i].equalsIgnoreCase("-d"))...
@@ -20,16 +22,16 @@ int main(int argc, char *argv[])
             if (i + 1 < argc) //cheking for the DIR in [-d DIR]
             {
                 input_directory = argv[i+1];
-                folder = opendir(input_directory);
+                folder = opendir(input_directory); 
                 if (!folder)
                 {
-                    pfrintf(stderr, "Error opening specified folder");
+                    fprintf(stderr, "Error opening specified folder");
                     return 1;
                 }
                 closedir(folder);
                 i++; //skipping a step 
             } else {
-                pfrintf(stderr, "You need to specify a diretory after -d or --dir.\n");
+                fprintf(stderr, "You need to specify a diretory after -d or --dir.\n");
                 return 1;
             }    
         }
@@ -53,12 +55,10 @@ int main(int argc, char *argv[])
                 closedir(folder);//directory already exists
             }
         }
-       
     }
 }
 
-    
-    
+    //--------------------------------------------------------------------------//
     if (output_directory == NULL) //Will create the default folder in the current working directory  
     {
         char *username = getenv("USER"); //I could ask for the user name, this simply gets it from the system. 
@@ -70,18 +70,27 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Error: No input directory provided (use -d or --dir).\n");
             return 1;
         }
-        char *last_slash = strrchr(input_directory, '/'); //finiding where the last slash is becasue input base follows that
-        char input_base[256];
-        if (last_slash)
+        char *last_slash = strrchr(input_directory, '/'); //finiding where the last slash is becasue destination directory follows that
+        char input_base[256]; //the destination directory will be stored here
+        if (last_slash) 
         {
-            strcpy(input_base, last_slash + 1);
+            strcpy(input_base, last_slash + 1); //if there is a last slash then I want to copy then everything after last slash to the input_base[]
         } else {
-            strcpy(input_base, input_directory);
+            strcpy(input_base, input_directory); //-------// review
         }
         char new_dir[256];
         //sprintf(new_dir, "%s_%s_%s", input_base, username, studentid);
         snprintf(new_dir, sizeof(new_dir), "%s_%s_%s", input_base, username, studentid); //using snprintf instead to prevent buffer overflow
         mkdir(new_dir, 0777); //mkdir logic found here https://www.delftstack.com/howto/c/mkdir-in-c/
         //Everyone has read and write permissions
-    return 0;  
+    }
+
+    //Make a loop that cycles through the directory and opens each file     
+   
+        
+
+
+
+   return 0; 
 }
+
